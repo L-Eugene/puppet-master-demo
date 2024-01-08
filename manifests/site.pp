dcee1ae15ed168892cccd72e8b951b8ebc898a70
Eugene Lapeko <eugene@lapeko.info>
2024-01-08 18:47:25 +0200

    Change password on slave2

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,6 +34,7 @@ node slave2.puppet {
   notify { "Running os ${facts['os']['name']} version ${facts['os']['release']['full']}": }
 
   class { 'dev_user':
-    user_name => 'prod_user'
+    user_name => 'prod_user',
+    password  => '$1$82O96PFM$8avkFBH/CitHmrE9rEtjb1'
   }
 }
