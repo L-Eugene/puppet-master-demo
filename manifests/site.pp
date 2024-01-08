b0e9641d6df61ff1e1a5251b7b12c71c8bba4b5c
Eugene Lapeko <eugene@lapeko.info>
2024-01-08 18:55:18 +0200

    Create group if not exist

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,10 @@
 class dev_user (
   String $user_name = 'demo_user',
   String $password  = '$1$Gdi1eg83$lCTP35NrycMk.MO7WQ5Ut1',
-  Array $groups = ['wheel', 'adm']
+  Array $groups = ['wheel', 'adm', 'developers']
 ) {
+  group { $groups:}
+
   user { $user_name:
     ensure => present,
     managehome => true,
