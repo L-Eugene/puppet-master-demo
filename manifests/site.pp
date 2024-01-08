731db2fd5808d89377f5bf9e3c489ea69ec59f40
Eugene Lapeko <eugene@lapeko.info>
2024-01-08 18:41:50 +0200

    Add groups parameter

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,12 @@
 class dev_user (
-  $user_name = 'demo_user',
-  $password  = '$1$Gdi1eg83$lCTP35NrycMk.MO7WQ5Ut1'
+  String $user_name = 'demo_user',
+  String $password  = '$1$Gdi1eg83$lCTP35NrycMk.MO7WQ5Ut1',
+  Array $groups = ['wheel', 'sudo']
 ) {
   user { $user_name:
     ensure => present,
     managehome => true,
-    groups => ['wheel'],
+    groups => $groups,
     password => $password,
   }
 }
